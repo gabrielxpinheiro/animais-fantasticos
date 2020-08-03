75e1e511ffb514ec82e3690012e435ca2a0fea5d
Gabriel Pinheiro <gabrielpinheiro77@gmail.com>
2020-08-03 17:40:34 -0300

    accordion faq

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
-}
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+  }
 
-tabContent[0].classList.add("ativo");
+  tabContent[0].classList.add("ativo");
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo");
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", () => activeTab(index));
   });
-  tabContent[index].classList.add("ativo");
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
 
-tabMenu.forEach((item, index) => {
-  item.addEventListener("click", () => activeTab(index));
+initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  const topo = section.offsetTop;
+  window.scrollTo({
+    top: topo,
+    behavior: "smooth",
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
 });
