beb21c2f50facd48c49ab5abfb10e972d5c1a9bc
Gabriel Pinheiro <gabrielpinheiro77@gmail.com>
2020-08-03 17:47:09 -0300

    scroll suave

--- script.js
+++ script.js
@@ -41,19 +41,30 @@ function initAccordion() {
 
 initAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
-  const topo = section.offsetTop;
-  window.scrollTo({
-    top: topo,
-    behavior: "smooth",
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    // section.scrollIntoView({
+    //   behavior: "smooth",
+    //   block: "start",
+    // });
+
+    // forma alternativa (scrollto caso funcione com objeto de parametro)
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
 
-linksInternos.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
+initScrollSuave();
